2353ff9633c4791ba717a72c658e9852a874f50c
mohdkaif0707 <mohammadkaif2140@gmail.com>
2026-07-05 11:59:08 +0530

    cloudinary and middleware done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name :process.env.CLOUDINARY_CLOUD_NAME,
+    api_key :  process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        // Upload the file to Cloudinary
+        const response =await cloudinary.uploader.upload(localFilePath,{
+            resource_type : "auto",
+        })
+        // file has been uploaded successfully
+        console.log("File uploaded to Cloudinary successfully",response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // remove the locally temporary file if upload fails
+        return null;
+    }
+};
+
+
+
+export {uploadOnCloudinary};

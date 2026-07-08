fcb4137225710d23c9bace7f03e35a3fdabe36fc
mohdkaif0707 <mohammadkaif2140@gmail.com>
2026-07-08 19:36:17 +0530

    authmiddleware added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,9 @@
 import {Router} from "express";
-import {registerUser} from "../controllers/user.controller.js";
+import {logoutUser, registerUser} from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+
+
 const  router = Router();
 
 router.route("/register").post(
@@ -15,5 +18,12 @@ router.route("/register").post(
         }
     ]),
     registerUser);
+
+router.route("/login").post(loginUser)
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
+
+
 
 export default router;
